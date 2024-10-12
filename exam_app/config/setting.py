b2cf1fcd4b1ee53ed1cd79c172370477dbadbc69
shamisen-cat <176907131+shamisen-cat@users.noreply.github.com>
2024-10-12 14:46:11 +0900

    refactor: avoid redundancy

--- exam_app/config/setting.py
+++ exam_app/config/setting.py
@@ -1,9 +1,9 @@
 """
 設定ファイルの読み込み
 
-Classes
--------
-JsonFile
+Attributes
+----------
+JsonFile : class
     JSONファイルから設定値を読み込み、設定値を定義する。
 
 """
@@ -16,18 +16,17 @@ from collections.abc import Callable
 from pathlib import Path
 from typing import Final, TypeVar
 
-CLS = TypeVar("CLS", bound=Callable)
+C = TypeVar("C", bound=Callable)
 
 # JSONファイルのファイル名
 FILE_NAME: Final[str] = "setting.json"
 
 
-def singleton(cls: CLS) -> CLS:
-    """インスタンスの生成"""
-    __instance: JsonFile | None = None
+def singleton(cls: C) -> C:  # noqa: D103
+    __instance = None
 
     @functools.wraps(cls)
-    def inner(*args, **kwargs) -> JsonFile:
+    def inner(*args, **kwargs):  # noqa: ANN202
         nonlocal __instance
         if __instance is None:
             __instance = cls(*args, **kwargs)
@@ -54,10 +53,10 @@ class JsonFile:
 
         Parameters
         ----------
-        default_settings : dict[str, int | str] | None
-            デフォルトの設定値, by default None
-        dir_layer : int
-            JSONファイルのディレクトリ階層, 0: カレントディレクトリ, 1: 親ディレクトリ, by default 0
+        default_settings : dict[str, int | str] | None, default None
+            デフォルトの設定値
+        dir_layer : int, default 0
+            JSONファイルのディレクトリ階層, 0: カレントディレクトリ, 1: 親ディレクトリ
 
         """
         if default_settings is None:
@@ -82,35 +81,38 @@ class JsonFile:
             with Path.open(file, "w", encoding="UTF-8") as f:
                 json.dump(default_settings, f, indent=4)
         except json.decoder.JSONDecodeError:
-            print("Failed to read settings from JSON File.")
+            print("Failed to read settings from JSON file.")
             settings = {}
 
         self.__settings: dict[str, int | str] = settings
 
     @property
-    def settings(self) -> dict[str, int | str]:
-        """設定値"""
+    def settings(self) -> dict[str, int | str]:  # noqa: D102
         return self.__settings
 
 
 if __name__ == "__main__":
-    settings_1: dict[str, int | str] = {"font": "", "font_size": 18}
-    settings_2: dict[str, int | str] = {"font": "HackGen", "font_size": 12, "width": 640}
+    settings_1: dict[str, int | str] = {"font_name": "", "font_size": 18}
+    settings_2: dict[str, int | str] = {"font_name": "HackGen", "font_size": 12, "width": 640}
 
     print("Generating an instance.")
-    json_file_1: JsonFile = JsonFile(settings_1)
-    json_file_2: JsonFile = JsonFile(settings_2)
-    json_file_3: JsonFile = JsonFile()
-    print("json_file_1.settings :", json_file_1.settings)
-    print("json_file_2.settings :", json_file_2.settings)
-    print("json_file_3.settings :", json_file_3.settings)
-    print("json_file_1 is json_file_2 :", json_file_1 is json_file_2)
-    print("json_file_1 is json_file_3 :", json_file_1 is json_file_3)
+    instance_1: JsonFile = JsonFile(settings_1)
+    instance_2: JsonFile = JsonFile(settings_2)
+    instance_3: JsonFile = JsonFile()
+
+    print("settings_1:", instance_1.settings)
+    print("settings_2:", instance_2.settings)
+    print("settings_3:", instance_3.settings)
+
+    print("instance_1 is instance_2:", instance_1 is instance_2)
+    print("instance_1 is instance_3:", instance_1 is instance_3)
 
     print("Modifying and Adding settings.")
-    json_file_1.settings["font"] = "HackGen"
-    json_file_2.settings["font_size"] = 12
-    json_file_3.settings["width"] = 640
-    print("json_file_1.settings :", json_file_1.settings)
-    print("json_file_1 is json_file_2 :", json_file_1 is json_file_2)
-    print("json_file_1 is json_file_3 :", json_file_1 is json_file_3)
+    instance_1.settings["font_name"] = "HackGen"
+    instance_2.settings["font_size"] = 12
+    instance_3.settings["width"] = 640
+
+    print("settings_1:", instance_1.settings)
+
+    print("instance_1 is instance_2:", instance_1 is instance_2)
+    print("instance_1 is instance_3:", instance_1 is instance_3)
