4d297d77704d1a08e27aa4ce1cc38927bdf642e7
shamisen-cat <176907131+shamisen-cat@users.noreply.github.com>
2024-11-04 21:53:59 +0900

    change: Move exception classes to the external file.

--- exam_app/config/setting.py
+++ exam_app/config/setting.py
@@ -6,20 +6,21 @@ SettingDict : TypedDict
     Define value types for the settings.
 DEFAULT_SETTINGS : Final[SettingDict]
     If no JSON file for the settings, the following settings will be used.
-JsonSchemaError : Exception
-    Base class for all schema errors when read the JSON file.
-RequiredKeysError : JsonSchemaError
-    If no required keys in the JSON file.
-UnwantedKeysError : JsonSchemaError
-    If unwanted keys in the JSON file.
-InvalidValueTypeError : JsonSchemaError
-    If there is an invalid value type in the JSON file.
 
 """
 
 import json
+from collections.abc import Callable
+from functools import wraps
 from pathlib import Path
-from typing import Any, Final, TypedDict
+from typing import Any, Final, TypedDict, TypeVar
+
+if __name__ == "__main__":
+    import exception as ex
+else:
+    from . import exception as ex
+
+F = TypeVar("F", bound=Callable)
 
 
 class SettingDict(TypedDict):
@@ -44,54 +45,43 @@ DEFAULT_SETTINGS: Final[SettingDict] = {
 }
 
 
-class JsonSchemaError(Exception):
-    """Base class for all schema errors when read the JSON file."""
+def validate_settings(func: F) -> F:  # noqa: D103
+    @wraps(func)
+    def inner(*args, **kwargs) -> SettingDict | None:
+        """Validate the setting values."""
+        settings: SettingDict | None = func(*args, **kwargs)
+        if settings:
+            try:
+                file_name: str = settings["file_name"]
+                if not file_name:
+                    raise ex.EmptyFileNameError
+
+                font_size: int = settings["font_size"]
+                if font_size < 0:
+                    raise ex.InvalidFontSizeError(font_size)
+
+                width: int = settings["width"]
+                if width < 0:
+                    raise ex.InvalidWidthError(width)
+
+                height: int = settings["height"]
+                if height < 0:
+                    raise ex.InvalidHeightError(height)
+
+                padding: int = settings["padding"]
+                if padding < 0:
+                    raise ex.InvalidPaddingError(padding)
+            except ex.SettingValueError as e:
+                print(type(e), e, sep="\n")
+                settings = None
+
+        return settings
+
+    return inner  # type: ignore  # noqa: PGH003
 
 
-class RequiredKeysError(JsonSchemaError):  # noqa: D101
-    def __init__(self, keys: set[str]) -> None:
-        """If no required keys in the JSON file.
-
-        Parameters
-        ----------
-        keys : set[str]
-            Required keys
-
-        """
-        super().__init__(f"No required keys in the JSON file: {keys}")
-
-
-class UnwantedKeysError(JsonSchemaError):  # noqa: D101
-    def __init__(self, keys: set[str]) -> None:
-        """If unwanted keys in the JSON file.
-
-        Parameters
-        ----------
-        keys : set[str]
-            Unwanted keys
-
-        """
-        super().__init__(f"Unwanted keys in the JSON file: {keys}")
-
-
-class InvalidValueTypeError(JsonSchemaError):  # noqa: D101
-    def __init__(self, key: str, value_type: type, default_type: type) -> None:
-        """If there is an invalid value type in the JSON file.
-
-        Parameters
-        ----------
-        key : str
-            Setting key
-        value_type : type
-            Invalid value type
-        default_type : type
-            Value type in 'DEFAULT_SETTINGS'
-
-        """
-        super().__init__(f"'{key}' {default_type} is an invalid value in the JSON file: {value_type}")
-
-
-def read_json_file(file_name: str, dir_layer: int = 1) -> SettingDict | None:
+@validate_settings
+def load_settings(file_name: str, dir_layer: int = 1) -> SettingDict | None:
     """Load settings from the JSON file and return the settings.
 
     Parameters
@@ -111,7 +101,6 @@ def read_json_file(file_name: str, dir_layer: int = 1) -> SettingDict | None:
 
     parent: Path = Path(__file__).resolve().parents[dir_layer]
     file: Path = parent.joinpath(file_name)
-
     try:
         with Path.open(file, encoding="UTF-8") as f:
             json_settings: dict[str, Any] = json.load(f)
@@ -121,39 +110,34 @@ def read_json_file(file_name: str, dir_layer: int = 1) -> SettingDict | None:
         json_keys: set[str] = set(json_settings.keys())
         default_keys: set[str] = set(DEFAULT_SETTINGS.keys())
 
-        if json_keys == default_keys:
-            for key, value in json_settings.items():
-                value_type: type = type(value)
-                default_type: type = type(DEFAULT_SETTINGS.get(key))
+        required_keys: set[str] = default_keys.difference(json_keys)
+        if required_keys:
+            raise ex.RequiredKeysError(required_keys)
 
-                if value_type is not default_type:
-                    raise InvalidValueTypeError(key=key, value_type=value_type, default_type=default_type)
-        else:
-            required_keys: set[str] = default_keys.difference(json_keys)
-            unwanted_keys: set[str] = json_keys.difference(default_keys)
+        unwanted_keys: set[str] = json_keys.difference(default_keys)
+        if unwanted_keys:
+            raise ex.UnwantedKeysError(unwanted_keys)
 
-            if required_keys:
-                raise RequiredKeysError(keys=required_keys)
-
-            if unwanted_keys:
-                raise UnwantedKeysError(keys=unwanted_keys)
+        for key, value in json_settings.items():
+            value_type: type = type(value)
+            required_type: type = type(DEFAULT_SETTINGS.get(key))
+            if value_type is not required_type:
+                raise ex.InvalidTypeError(key, value_type, required_type)
 
         settings = json_settings  # type: ignore  # noqa: PGH003
     except FileNotFoundError:
         with Path.open(file, "w", encoding="UTF-8") as f:
             json.dump(DEFAULT_SETTINGS, f, indent=4)
-
         settings = DEFAULT_SETTINGS
     except json.decoder.JSONDecodeError as e:
         print(type(e), "Failed to load settings from the JSON file.", sep="\n")
-    except JsonSchemaError as e:
+    except ex.JsonSchemaError as e:
         print(type(e), e, sep="\n")
 
     return settings
 
 
 if __name__ == "__main__":
-    settings: SettingDict | None = read_json_file("setting.json")
-
+    settings: SettingDict | None = load_settings("setting.json")
     if settings:
         print(settings)
